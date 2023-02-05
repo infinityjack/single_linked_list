249ce4321e112579e6acb466e78dcdab73675c45
jack_cizon <105433087+infinityjack@users.noreply.github.com>
2023-02-06 01:02:08 +0800

    Update list_main.c

--- list_main.c
+++ list_main.c
@@ -41,11 +41,12 @@ PRIVATE void double_operations(void)
     
     double_head(dL);
 
-    double_append(dL, 3.0);
-    double_append(dL, 4.0);
+    double_append(dL, 0.0);
+    double_append(dL, 0.0);
+    double_append(dL, 0.0);
 
     double_size(dL) == dL->size ?
-        printf("double size: %d\n", dL->size) : puts("not equal");
+        printf("\ndouble size: %d\n", dL->size) : puts(""),puts("not equal");
 
     double_traverse(dL);
 }
@@ -57,21 +58,23 @@ PRIVATE void char_operations(void)
 
     char_append(cL, 'c');
     char_head(cL) == NULL ?
-        puts("char head is null") : puts("char head is not null");
+        puts(""),puts("char head is null") : puts(""),puts("char head is not null");
 
-    char_append(cL, 'd');
+    char_append(cL, 'o');
+    char_append(cL, 'v');
+    char_push(cL, ' ');
+    char_push(cL, 'I');
     char_append(cL, 'e');
-    char_push(cL, 'b');
-    char_push(cL, 'a');
-    char_append(cL, 'f');
-    char_insert(cL, 3, 'x');
+    char_insert(cL, 3, 'L');
     char_insert(cL, 0, '0');
     char_insert(cL, 0, '0');
     char_remove(cL, '0');
-
-
+    char_append(cL, ' ');
+    char_append(cL, 'C');
     char_delete(cL, 0);
     char_delete(cL, 2);
+    char_push(cL, '\n');
+    char_append(cL, '\n');
 
     char_traverse(cL);
 }
@@ -84,15 +87,17 @@ PRIVATE void string_operations(void)
     string_append(sL, "whatis");
     string_append(sL, "myip");
     string_append(sL, ".com");
+    string_append(sL, "\n\n");
     string_push(sL, "www.");
     string_insert(sL, 0, "https:");
     string_insert(sL, 1, "//");
-    string_insert(sL, 0, "nslookup duckduckgo.com");
-    string_append(sL, "qqq");
-    string_append(sL, "lll");
-    string_append(sL, "rrr");
+    string_insert(sL, 6, "\nnslookup duckduckgo.com");
+    string_push(sL, "\n");
+    string_push(sL, "find you ip address ?");
+    string_push(sL, "\n");
 
-    puts(string_get(sL, 0));
+    string_get(sL, 0);
+    system("");
     printf("%d\n",(string_index(sL, "//")));
     string_pop(sL);
 
